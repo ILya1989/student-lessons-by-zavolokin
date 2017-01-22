ce525cd197896e4b6c5709bd04ff40a75e076919
ILya1989 <zavolokin.praim@gmail.com>
2017-01-23 02:28:56 +0300

    Добавил Пакет а так же клас вызывающий функцию

--- SendBox/src/main/java/Point.java
+++ SendBox/src/main/java/Point.java
@@ -6,17 +6,22 @@ public class Point {
 
 
 
-        public static void main(String args[]){
+        public static void main(String args[]) {
             double x1 = 16;
-            double x2 =22 ;
-            double P1= x1;
-            double P2=x2;
+            double x2 = 22;
+            System.out.println("Растояние равно=" + distans( x1,  x2));
+           }
 
 
-            double c = Math.sqrt(x1*x1 + x2*x2);
-            System.out.println(c);
-            double distans=(Math.round((c)));
-            System.out.println("Растояние равно="+ distans);
+
+            public static double distans( double x1, double x2) {
+                double c = Math.sqrt(x1 * x1 + x2 * x2);
+                System.out.println(c);
+                double distans = (Math.round((c)));
+                System.out.println("Растояние равно=" + distans);
+                return c;
+            }
+
 
 
 
@@ -29,5 +34,5 @@ public class Point {
 
 
 
-    }
+
 
